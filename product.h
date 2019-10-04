530474014ef5940ff2eb53f97f857953b1a0762d
MOping99 <49947442+MOping99@users.noreply.github.com>
2019-10-04 07:48:25 -0500

    Update product.h
    0.5
    
    -all code is untested though they should work with few errors if any and be relatively simple to fix
    -coding stops at multiplication since I was unsure of how to continue

--- product.h
+++ product.h
@@ -1,11 +1,23 @@
 // MCS 360 Project Two by <Mikael & Victor>
 
 #include "monomial.h"
+#include <list>
 
 class Product{
 
   private:
-    list<Monomial>& product;
+    list<Monomial> prod;
 
-
+  public:
+    Product(){list<Monomial> prod;};
+    Product(Monomial mono){list<Monomial> prod = {mono};};
+    Product(Product prod){
+      list<Monomial> new_prod(prod);
+      new_prod.sort();
+    };
+  
+    int size(){return (this -> size())};
+  
+    //Unsure of how to do multiplication as of yet
+  
 };
