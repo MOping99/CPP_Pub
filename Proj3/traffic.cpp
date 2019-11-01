4360b82aeb911af39c414ed153e120365437520b
Mikael_O <moping2@uic.edu>
2019-10-31 19:52:44 -0500

    1.0
    -hopefully a working traffic that gets full credit

--- Proj3/traffic.cpp
+++ Proj3/traffic.cpp
@@ -17,6 +17,7 @@ int main(){
 
   vector<Lane> Crossings;
   vector<double> Waits;
+  vector<double> AverageWait;
 
   for(int i = 0; i < 12; i++){
 
@@ -40,16 +41,64 @@ int main(){
   cout << "Average waiting times for each queue :" << endl;
 
 
-  Signal states = Signal();
+  Signal lights = Signal();
   int s, e;
 
-  s = test.get_state();//Stores state.
-  e = test.get_elapsed();//Stores elsaped.
+  s = lights.get_state();//Stores state.
+  e = lights.get_elapsed();//Stores elapsed.
+
+  int i = 0;
+
+  verbose = true;
+
+  while(e < 5){
+
+    if(s == 0){
+      cout << "green for left traffic from north and south, red for all others\n";
+
+      for(i; i < 2; i++){
+        int lanesize = Crossings[i].size();
+        Waits.push_back(Crossings[i].pop(e, time, verbose));
+        AverageWait.push_back(Waits[i]/lanesize);
+      }
+    }
+    if(s == 1){
+      cout << "green for straight and right going traffic from north and south,\nred for all others\n";
+
+      for(i; i < 6; i++){
+        int lanesize = Crossings[i].size();
+        Waits.push_back(Crossings[i].pop(e, time, verbose));
+        AverageWait.push_back(Waits[i]/lanesize);
+      }
+    }
+    if(s == 2){
+      cout << "green for left traffic from west and east, red for all others\n";
+
+      for(i; i < 8; i++){
+        int lanesize = Crossings[i].size();
+        Waits.push_back(Crossings[i].pop(e, time, verbose));
+        AverageWait.push_back(Waits[i]/lanesize);
+      }
+    }
+    if(s == 3){
+      cout << "green for straight and right going traffic from west and east,\nred for all others\n";
+
+      for(i; i < 12; i++){
+        int lanesize = Crossings[i].size();
+        Waits.push_back(Crossings[i].pop(e, time, verbose));
+        AverageWait.push_back(Waits[i]/lanesize);
+      }
+    }
+
+    lights.next();
+    s = lights.get_state();//Stores state.
+    e = lights.get_elapsed();//Stores elapsed.
+  }
 
 
   for(int i = 0; i < 12; i++){
 
-    cout << "queue [" << i << "] : " << Waits[i]/Crossings[i].size() << endl;
+    cout << "queue [" << i << "] : " << AverageWait[i] << endl;
 
   }
 
