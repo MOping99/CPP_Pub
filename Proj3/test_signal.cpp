24922fc2e1c1dd50e50edc8be4b1491d6333e1a9
Mikael_O <moping2@uic.edu>
2019-10-31 19:52:17 -0500

    1.0
    -fixed a small printing error

--- Proj3/test_signal.cpp
+++ Proj3/test_signal.cpp
@@ -16,26 +16,26 @@ int main()
 	{
 		s = test.get_state();//Stores state.
 		e = test.get_elapsed();//Stores elsaped.
-		
+
 		cout << "State : " << s << "  ";//Outputs the state value.
 		cout << "elasped time : " << e << '\n';//Outputs the elasped value.
-		
+
 		if(s == 0)//Outputs appropriate message depending on state value.
 			cout << "green for left traffic from north and south, red for all others\n";
 		else if(s == 1)
 			cout << "green for straight and right going traffic from north and south,\nred for all others\n";
-		else if(s == 3)
+		else if(s == 2)
 			cout << "green for left traffic from west and east, red for all others\n";
 		else
 			cout << "green for straight and right going traffic from west and east,\nred for all others\n";
-		
+
 		cout << "Move to the next state ? (y/n) ";//Ask users for a y/n input.
 		cin >> choice;//Takes in a y/n input.
 		assert(choice == 'y' || choice == 'n');//Asserts that choice must be either 'y' or 'n'.
-		
+
 		if(choice == 'y')//Uses 'next' method if  choice is 'y'.
 			test.next();//Goes to next state and interates elapsed.
 	}
-	
+
 	return 0;
 }
