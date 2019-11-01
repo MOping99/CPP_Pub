cb347eb40cb141e495a0341337a9fc8e0676c502
BlazeRed251 <56836635+BlazeRed251@users.noreply.github.com>
2019-10-31 19:35:17 -0500

    Update lane.h

--- Proj3/lane.h
+++ Proj3/lane.h
@@ -3,18 +3,18 @@
 #ifndef LANE_H
 #define LANE_H
 
-#include <queue>
+#include <queue>//To use the queue data structure to store arrival times
 
 class Lane{
 	private:
-    std::queue<double> arrivals;
+		std::queue<double> arrivals;//Used to store arrival times.
 
 	public:
-		Lane(int time, int lambda, bool verbose);
+		Lane(int time, int lambda, bool verbose);//Constructs a Lane object using time, lambda and verbose variables as inputs to generate arrival times.
 
-    int size();
+		int size();//Returns the size of the private member arrivals.
 
-    double pop(int start, int end, bool verbose);
+		double pop(int start, int end, bool verbose);//Prints and removes the arrival times between the parameters start and end.
 
 };
 
