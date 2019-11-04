c3d245b469dab3b5c407f2d04f3529ba06ad8a93
Mikael_O <moping2@uic.edu>
2019-11-04 10:16:10 -0600

    0.1.2
    -imported packages 
    -started work on functions

--- Proj4/board.h
+++ Proj4/board.h
@@ -3,13 +3,24 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+
+using namespace std;
+
 class Board{
 
   private:
+    int number;
 
 
   public:
-
+    friend ostream &operator<<(ostream &os, const Monomial &Obj){
+      os << Obj.symbol << "^" << Obj.exponent;
+      return os;
+    };
 
 }
 
