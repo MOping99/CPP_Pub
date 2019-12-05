dcbbd0630427b1d978bd9b6b664ff4c8f59bd342
MOping99 <moping2@uic.edu>
2019-12-05 11:09:05 -0600

    0.9.8
    -all levels have been refactored and commented

--- Proj5/Final_TurnIn/player.cpp
+++ Proj5/Final_TurnIn/player.cpp
@@ -19,42 +19,6 @@ int Player::get_level() //Function used to retrieve the level of strategy
 int Player::get_score() //Function used to retrieve the score of a player
 {return score;}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //The most important function which actually lets a player have their turn
 //It begins by first telling all present who is playing as shown by the cout
 //then it breaks people apart based on what their strategy and plays through
@@ -68,24 +32,24 @@ void Player::play(){
 		//We define the integers that will be used to pick a coordinate
 		int rowind, colind;
 
-			//We take in the users coordinate choice and use that to see if they can make a chain
-			cout << "-> give a row index : ";
-			cin >> rowind;
-			cout << "-> give a column index : ";
-			cin >> colind;
+		//We take in the users coordinate choice and use that to see if they can make a chain
+		cout << "-> give a row index : ";
+		cin >> rowind;
+		cout << "-> give a column index : ";
+		cin >> colind;
 
-			//If a chain has been created we give the user a point and tell them that it succeeded and then show
-			//the new altered board to them, before we've removed the chain
-			if(board -> swap(rowind, colind)){
-				score++;
-				cout << "The swap succeeded. Your score is " << score << endl;
-				cout << "Current Board :\n" << *board;
-			}
-			//Or we tell them that they failed to make a chain making move on to the chain destruction at the
-			//end of the function
-			else{
-				cout << "The swap failed." << endl;
-				}
+		//If a chain has been created we give the user a point and tell them that it succeeded and then show
+		//the new altered board to them, before we've removed the chain
+		if(board -> swap(rowind, colind)){
+			score++;
+			cout << "The swap succeeded. Your score is " << score << endl;
+			cout << "Current Board :\n" << *board;
+		}
+		//Or we tell them that they failed to make a chain making move on to the chain destruction at the
+		//end of the function
+		else{
+			cout << "The swap failed." << endl;
+		}
 
 }
 
@@ -93,6 +57,8 @@ void Player::play(){
 	    //We define the integers that will be used to pick a coordinate
 	  	int rowind, colind;
 
+			//We make the output look like that of a real person however instead of actually taking in an input
+			//we use rand() based on the dimensions of the board to pick the coordinates
 			cout << "-> give a row index : ";
 			rowind = rand() % board -> dimension();
 			cout << rowind << endl;
@@ -109,96 +75,119 @@ void Player::play(){
 			}
 			else{
 				cout << "The swap failed." << endl;
-				}
+			}
 
   }
 
 	if(this -> get_level() == 1){
-	bool breaking = false;
-	int row, col;
-	for(int i = 0; i < board -> dimension(); i++){
-		for(int j = 0; j < board -> dimension(); j++){
-			if(board -> swap(i,j)){
-				breaking = true;
-				row = i;
-				col = j;
+		//Like before we define the integers that will be used to pick a coordinate however we also add
+		//a boolean breaking, to notify us that we found a chain and wish to stop looking through the board
+		bool breaking = false;
+		int row, col;
+
+		//As we loop through th board we contantly check to see wether a swap can be made, since swap returns
+		//a boolean we use its return as a trigger to make breaking true as well as stroing the coordinate it
+		//occured at
+		for(int i = 0; i < board -> dimension(); i++){
+			for(int j = 0; j < board -> dimension(); j++){
+				if(board -> swap(i,j)){
+					breaking = true;
+					row = i;
+					col = j;
+				}
+				//As breaking is set to true it triggers a break twice so that the nested for loop is existed
+				if(breaking){
+					break;
+				}
 			}
 			if(breaking){
 				break;
 			}
 		}
-		if(breaking){
-			break;
-		}
-	}
 
-	if(breaking){
-		cout << "-> give a row index : " << row << endl;
-		cout << "-> give a column index : " << col << endl;
+		//Since it's not nessecarily true we can loop through the board and find possible chain, we use breaking
+		//being true to print out that a swap was made and showing it to the user
+		if(breaking){
+			cout << "-> give a row index : " << row << endl;
+			cout << "-> give a column index : " << col << endl;
 			score++;
 			cout << "The swap succeeded. Your score is " << score << endl;
 			cout << "Current Board :\n" << *board;
-	}
-	else{
-		cout << "The swap failed. This board contains no more possible chains!" << endl;
-		assert(0 == 1);
-	}
-
+		}
+		//And the opposite as a way to tell them their are no chains, and forcing the program to end by asserting
+		//a clearly false statment
+		else{
+			cout << "The swap failed. This board contains no more possible chains!" << endl;
+			assert(0 == 1);
+		}
 
 }
 
 	if(this -> get_level() == 2){
 
-	pair<int,int> best (-1,-1);
-	int bestchain = -1;
+		//We use pairs here for the first time as both a way to store our coordinates and gain
+		//some experience with it, we also have an integer called bestchain which is clearly
+		//impossible, the same way our coordinate is, as a way to compare it to any future values
+		//and then alsways pick those future values
+		pair<int,int> best (-1,-1);
+		int bestchain = -1;
 
-	for(int x = 0; x < board -> dimension(); x++){
-		for(int y = 0; y < board -> dimension(); y++){
-			Board copy = *board;
+		//This loop goes through the base board all players share, however instead of opperating
+		//on it, we make a complete non-pointer copy that we will effect everytime
+		for(int i = 0; i < board -> dimension(); i++){
+			for(int j = 0; j < board -> dimension(); j++){
+				Board copy = *board;
 
-			if(copy.swap(x,y)){
-				int hs = copy.hcount(x, y);
-				int vs = copy.vcount(x, y);
+				//Since swap can disfigure a board by making a chain, and his no way to unmake that chain
+				//we swap on out copy and store the resulting chains length in both the horizontl axis as
+				//well as the vertical axis
+				if(copy.swap(i,j)){
+					int hs = copy.hcount(i, j);
+					int vs = copy.vcount(i, j);
 
-				if(hs >= vs){
-					if(hs >= bestchain){
-						bestchain = hs;
-						best.first = x;
-						best.second = y;
+					//The next thing we do is  a series of nested if statments to know if this coordinate is
+					//the best in making a long chain. SInce the coordinate and the chain size is strored
+					//outside the loop, we are immune from accidentally deleting them as we loops
+
+					//The first if check to see if we should be making a horizantal or chain or a vertial one
+					if(hs >= vs){
+						//Given we need to make a horizantal chain, we see wether or not it's bigger then the best
+						//chain if it is bigger not only do we store it as the best possible chain but also store
+						//the coordinate that we found it in
+						if(hs >= bestchain){
+							bestchain = hs;
+							best.first = i;
+							best.second = j;
+						}
+					}
+					else{
+						//Given we need to make a vertical chain, we see wether or not it's bigger then the best
+						//chain if it is bigger not only do we store it as the best possible chain but also store
+						//the coordinate that we found it in
+						if(vs >= bestchain){
+							bestchain = vs;
+							best.first = i;
+							best.second = j;
+						}
 					}
 				}
-				else{
-					if(vs >= bestchain){
-						bestchain = vs;
-						best.first = x;
-						best.second = y;
-					}
-				}
+
 			}
-
-
-
 		}
-	}
 
-	bool breaking = false;
-	int row = best.first;
-	int col = best.second;
-	if(board -> swap(row, col)){
-		breaking = true;
-	}
-	if(breaking){
+		//We then move the stored variable of the pair into a row and column variable aptly named row and
+		//col and proceed to show the user his board and score changing. The reason we don't every check
+		//to make sure if it did find a chain, is because if no chains where made, row and col are both -1
+		//and fail a previous assertion the board has
+		int row = best.first;
+		int col = best.second;
+
+		board -> swap(row, col);
 		cout << "-> give a row index : " << row << endl;
 		cout << "-> give a column index : " << col << endl;
-			score++;
-			cout << "The swap succeeded. Your score is " << score << endl;
-			cout << "Current Board :\n" << *board;
-	}
-	else{
-		cout << "The swap failed. This board contains no more possible chains!" << endl;
-		assert(0 == 1);
-	}
-
+		score++;
+		cout << "The swap succeeded. Your score is " << score << endl;
+		cout << "Current Board :\n" << *board;
 
 }
 
